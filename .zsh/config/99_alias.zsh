2318c88c02d63b051b8ef5a431c663f42be660e1
Ditscher Christian (XC-DX/EAS3) <Christian.Ditscher@de.bosch.com>
2023-04-26 11:13:55 +0200

    added alias to ipv6-enable and ipv6-disable

--- .zsh/config/99_alias.zsh
+++ .zsh/config/99_alias.zsh
@@ -16,3 +16,7 @@ alias xl='ls -CF'
 alias branch='git branch --no-color --show-current'
 
 alias g='git'
+
+alias ipv6-disable='sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1 && sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1'
+alias ipv6-enable='sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0 && sudo sysctl -w net.ipv6.conf.default.disable_ipv6=0 && sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=0'
+
